d198874c135364c0fd00e020dfdfd7cc1c36f384
Warren Caulton <warrenpk1997@gmail.com>
2015-06-22 20:18:38 -0400

    Working Base

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SBAdmin2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult NotLoggedIn()
+        {
+            ViewBag.Message = "You must be logged-in to use this functionality";
+
+            return View();
+        }
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Flot()
+        {
+            ViewBag.Message = "Flot Chart Page";
+            return View();
+        }
+
+        public ActionResult Morris()
+        {
+            ViewBag.Message = "Morris.js Chart Page";
+            return View();
+        }
+
+        public ActionResult Tables()
+        {
+            ViewBag.Message = "Tables Page";
+            return View();
+        }
+
+        public ActionResult Forms()
+        {
+            ViewBag.Message = "Forms Page";
+            return View();
+        }
+
+        public ActionResult Panels()
+        {
+            ViewBag.Message = "Panels Page";
+            return View();
+        }
+
+        public ActionResult Buttons()
+        {
+            ViewBag.Message = "Buttons Page";
+            return View();
+        }
+
+        public ActionResult Notifications()
+        {
+            ViewBag.Message = "Notifications Page";
+            return View();
+        }
+
+        public ActionResult Typography()
+        {
+            ViewBag.Message = "Typography Page";
+            return View();
+        }
+
+        public ActionResult Icons()
+        {
+            ViewBag.Message = "Icons Page";
+            return View();
+        }
+
+        public ActionResult Grid()
+        {
+            ViewBag.Message = "Grid Page";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SBAdmin2.MvcApplication" Language="C#" %>
